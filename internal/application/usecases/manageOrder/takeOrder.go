86a58d634f8b4681a3e66cbeb1c772c9cccdd060
literally <literally_user@hotmail.com>
2025-12-22 22:07:55 +0300

    order usecase

---
+++ internal/application/usecases/manageOrder/takeOrder.go
@@ -0,0 +1,30 @@
+package manageOrder
+
+import (
+	"github.com/google/uuid"
+	"github.com/literally_user/gozon/internal/application/common/repositories"
+	applicationErrors "github.com/literally_user/gozon/internal/application/errors"
+)
+
+type TakeOrderInteractor struct {
+	Repository repositories.OrderRepository
+}
+
+func (i *TakeOrderInteractor) Execute(orderUUID uuid.UUID) error {
+	order, err := i.Repository.GetByUUID(orderUUID)
+	if err != nil {
+		return applicationErrors.ErrOrderNotFound
+	}
+
+	err = order.MarkAsTaken()
+	if err != nil {
+		return err
+	}
+
+	err = i.Repository.Update(order)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
